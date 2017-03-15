9aac743c5de3c7ba3a6981074ec5178ab06208a9
Erik Svedäng <erik.svedang@gmail.com>
2017-03-15 11:48:14 +0100

    Array.

--- src/Pearl1.hs
+++ src/Pearl1.hs
@@ -1,5 +1,8 @@
 module Pearl1 where
 
+import Data.Array
+
+-- Simplest solution
 type Nat = Int
 
 minfree :: [Nat] -> Nat
@@ -10,4 +13,7 @@ us \\ vs = filter (`notElem` vs) us
 
 ex1 = minfree [5, 10, 0, 4, 3, 2, 1]
 
+-- Array based solution
 
+search :: Array Int Bool -> Int
+search = length . takeWhile id . elems
