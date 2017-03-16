9660f419aa7d7088d9cb69200b5ee5510453d6d8
Erik Svedäng <erik.svedang@gmail.com>
2017-03-16 12:05:41 +0100

    accumArray-solution

--- src/Pearl1.hs
+++ src/Pearl1.hs
@@ -1,8 +1,9 @@
 module Pearl1 where
 
 import Data.Array
+import Data.List (partition)
 
--- Simplest solution
+-- Simplest solution --
 type Nat = Int
 
 minfree :: [Nat] -> Nat
@@ -13,7 +14,34 @@ us \\ vs = filter (`notElem` vs) us
 
 ex1 = minfree [5, 10, 0, 4, 3, 2, 1]
 
--- Array based solution
-
+-- Array based solution --
 search :: Array Int Bool -> Int
 search = length . takeWhile id . elems
+
+checklist :: [Int] -> Array Int Bool
+checklist xs = accumArray (||) False (0, n) assocList
+               where n = length xs
+                     assocList = (zip (filter (<= n) xs) (repeat True))
+
+ex2 = search (checklist [5, 10, 0, 4, 3, 2, 1])
+
+-- QUESTION: The 'accumulator' function (first arg to accumArray) is
+--           called for each item that exists in the alist, otherwise
+--           just the 'initial value' (False in this case) is used?!
+
+countlist :: [Int] -> Array Int Int
+countlist xs = accumArray (+) 0 (0, n) (zip xs (repeat 1))
+               where n = maximum xs
+                
+-- Divide & Conquer solution --
+
+-- (as ++ bs) \\ cs == (as \\ cs) ++ (bs \\ cs)
+-- as \\ (bs ++ cs) == (as \\ bs) \\ cs
+-- (as \\ bs) \\ cs == (as \\ cs) \\ bs
+
+-- minfree2 xs = if null ([0 .. b - 1] \\ us)
+--               then head ([b..] \\ vs)
+--               else head ([0..] \\ us)
+--   where (us, vs) = partition (< b) xs
+
+-- minfrom
